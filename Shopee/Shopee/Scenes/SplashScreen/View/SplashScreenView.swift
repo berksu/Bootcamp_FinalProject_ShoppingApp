d87d66a228dc54deb29f044f105ac2173029065e
Berksu Kısmet <berksukismet@gmail.com>
2022-10-26 00:13:36 +0300

    Refactore code

---
+++ Shopee/Shopee/Scenes/SplashScreen/View/SplashScreenView.swift
@@ -0,0 +1,71 @@
+//
+//  SplashScreenView.swift
+//  Shopee
+//
+//  Created by Berksu Kısmet on 25.10.2022.
+//
+
+import UIKit
+
+final class SplashScreenView: UIView{
+    
+    private let logoImageView: UIImageView = {
+       let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+       imageView.image = UIImage(named: "splashScreenLogo")
+       imageView.contentMode = .scaleAspectFit
+       return imageView
+   }()
+   
+    private let indicator: UIActivityIndicatorView = {
+       let indicatorView = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+       indicatorView.startAnimating()
+       return indicatorView
+   }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        if #available(iOS 13.0, *) {
+            backgroundColor = .systemBackground
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 150),
+            logoImageView.widthAnchor.constraint(equalToConstant: 150)
+        ])
+        
+        addSubview(indicator)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            indicator.topAnchor.constraint(equalTo: self.logoImageView.bottomAnchor, constant: 88),
+            indicator.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+        
+
+    }
+    
+     func animate(){
+        indicator.alpha = 0
+
+         let originalTransform = logoImageView.transform
+         let scaledTransform = originalTransform.scaledBy(x: 10.0, y: 10.0)
+         let scaledAndTranslatedTransform = scaledTransform.translatedBy(x: 0.0, y: 0.0)
+         UIView.animate(withDuration: 0.7, animations: {
+             self.logoImageView.transform = scaledAndTranslatedTransform
+        })
+        
+        UIView.animate(withDuration: 1.5) {
+            self.logoImageView.alpha = 0
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Shopee/Shopee/Scenes/SplashScreen/ViewController/SplashScreenViewController.swift
+++ Shopee/Shopee/Scenes/SplashScreen/ViewController/SplashScreenViewController.swift
@@ -9,66 +9,20 @@ import UIKit
 
 class SplashScreenViewController: UIViewController {
 
-    //    let splashView = SplashScreenView()
-        
-        private let logoImageView: UIImageView = {
-            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
-            imageView.image = UIImage(named: "splashScreenLogo")
-            imageView.contentMode = .scaleAspectFit
-            return imageView
-        }()
+    let splashView = SplashScreenView()
 
-        private let indicator: UIActivityIndicatorView = {
-            let indicatorView = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
-            indicatorView.startAnimating()
-            return indicatorView
-        }()
-
-        
+    override func loadView() {
+        super.loadView()
+        view = splashView
+    }
+    
         override func viewDidLoad() {
             super.viewDidLoad()
-            // Do any additional setup after loading the view.
-            if #available(iOS 13.0, *) {
-                view.backgroundColor = .systemBackground
-            } else {
-                // Fallback on earlier versions
-            }
-
-            view.addSubview(logoImageView)
-            logoImageView.center = view.center
-
-            view.addSubview(indicator)
-            indicator.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([
-                indicator.topAnchor.constraint(equalTo: self.logoImageView.bottomAnchor, constant: 88),
-                indicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
-            ])
         }
-        
-        override func viewDidLayoutSubviews() {
-            super.viewDidLayoutSubviews()
-            //logoImageView.center = view.center
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2 , execute: {self.animateView()})
-        }
-
-        func animateView(){
-            indicator.alpha = 0
-
-            UIView.animate(withDuration: 1) {
-                let size = self.view.frame.size.width * 3
-                let diffX = size - self.view.frame.size.width
-                let diffY = self.view.frame.size.height - size
-                
-                self.logoImageView.frame = CGRect(x: -(diffX/2),
-                                                  y: diffY/2,
-                                                  width: size,
-                                                  height: size)
-            }
-
-            UIView.animate(withDuration: 1.5) {
-                self.logoImageView.alpha = 0
-            }
-        }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.6 , execute: {self.splashView.animate()})
+    }
 }
 
