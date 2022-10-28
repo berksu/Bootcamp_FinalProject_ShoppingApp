7b998993f2cf4d9a1a38d546477287f60cdcadea
Berksu Kısmet <berksukismet@gmail.com>
2022-10-28 15:02:33 +0300

    Added firebase configuration and plist

--- Shopee/Shopee/App/Delegates/AppDelegate.swift
+++ Shopee/Shopee/App/Delegates/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SnapKit
 import IQKeyboardManagerSwift
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // MARK: -Firebase configuration
+        FirebaseApp.configure()
         
         // MARK: -IQKeyboard configuration
         IQKeyboardManager.shared.enable = true
