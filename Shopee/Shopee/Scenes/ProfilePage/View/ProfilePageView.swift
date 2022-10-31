8af6802a5d851c5d44eb7d3d2b61d83444c4e469
Berksu Kısmet <berksukismet@gmail.com>
2022-10-31 21:08:25 +0300

    Added UIColor extension for hex colors, added profile page label stack

--- Shopee/Shopee/Scenes/ProfilePage/View/ProfilePageView.swift
+++ Shopee/Shopee/Scenes/ProfilePage/View/ProfilePageView.swift
@@ -9,12 +9,47 @@ import UIKit
 
 final class ProfilePageView: UIView{
     
+    private let userNameLabel = {
+       let label = UILabel()
+        label.text = "Username"
+        label.font = UIFont(name: "Inter-Bold", size: 28)
+        return label
+    }()
+    
+    private let userEmailLabel = {
+       let label = UILabel()
+        label.text = "Email"
+        label.font = UIFont(name: "Inter-Regular", size: 14)
+        label.textColor =  UIColor(rgb: 0x808080, a: 1)
+        return label
+    }()
+    
+    private let userNameAndEmailStack:UIStackView = {
+        let stack = UIStackView()
+        stack.spacing = 4
+        stack.axis = .vertical
+        return stack
+    }()
+    
     init(){
         super.init(frame: .zero)
         backgroundColor = .white
+        
+        userNameAndEmailStack.addArrangedSubview(userNameLabel)
+        userNameAndEmailStack.addArrangedSubview(userEmailLabel)
+        
+        addSubview(userNameAndEmailStack)
+        userNameAndEmailStack.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top)
+            make.leading.equalTo(safeAreaLayoutGuide.snp.leading).offset(16)
+            make.trailing.equalTo(safeAreaLayoutGuide.snp.trailing).offset(-16)
+            make.height.equalTo(60)
+        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+
